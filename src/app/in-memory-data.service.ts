e54106485db611122b120ef9f0adfc8959a66646
a.sato <a.sato@pro-japan.co.jp>
2020-06-05 07:51:04 +0000

    first commit

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,32 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Hero } from './hero';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+    return {heroes};
+  }
+
+  // genIdメソッドをオーバーライドして、ヒーローが常にIDを持つようにする。
+  // ヒーローの配列がからの場合、以下のメソッドは最初の数（11）を返す。
+  // heroes配列がから出ない場合、以下のメソッドは最も高いものを返す。
+  // ヒーローID + 1
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
